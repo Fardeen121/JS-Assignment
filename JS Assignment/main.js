79d08ba9778eea70552766651a3f040c0abb9062
Fardeen121 <124420376+Fardeen121@users.noreply.github.com>
2023-06-28 13:00:50 +0530

    Add files via upload

---
+++ JS Assignment/main.js
@@ -0,0 +1,26 @@
+// 1  Write a javascript program to display 'Hello World' on the console.
+
+console.log("Hello World");
+
+
+// 2  Write a javascript program to add two numbers and display the result.
+
+var x = 20
+var y = 50
+
+console.log(x+y);
+
+// 3  Write a javascript program to check if a number is Even or Odd.
+
+var number = 344
+if(number % 2 === 0){
+    console.log('Even');
+}else{
+    console.log('Odd');
+}
+
+// 4  Write a javascript program to Convert celsius to Farenhite.
+
+var Farenhite = 50
+var celsius = (Farenhite * 5/9)+32
+console.log(celsius);
